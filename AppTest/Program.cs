9b5d7bef28b11e469b2bfecca1e9ed810c55d227
NaNoXumuk <famassamaf@gmail.com>
2024-08-08 20:04:08 +0900

    Some progs

--- AppTest/Program.cs
+++ AppTest/Program.cs
@@ -1,10 +1,13 @@
 ﻿//#define Calculator
 //#define ArrayChecker
 //#define FacebookLiker
-#define ReplaceLetters
+//#define Origami
+//#define HighandLow
+#define Test
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization.Formatters;
@@ -18,7 +21,7 @@ namespace AppTest
 internal class Program
 {
         static void Main(string[] args)
-        {
+    {
 #if Calculator
             float CacheResult;
             int CountMathIterations = 0, i, DoPlusMinus = 0;
@@ -85,41 +88,6 @@ internal class Program
         
 #endif
 
-#if ArrayChecker
-        static void ArrayChecker(string[] args)
-        {
-            int[] arr1 = new[]
-          {
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-            28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
-            54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-            80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99
-        }; // 100
-            Console.WriteLine(MissingNo(arr1));
-            Console.ReadKey();
-        }
-        public static int MissingNo(int[] nums)
-        {
-            int Count = 0, Missnumber = 0;
-            bool flag = false;
-            for (int i = 0; i < nums.Length + 1; i++)
-            {
-                for (int j = 0; j < nums.Length; j++)
-                {
-                    if (Count == nums[j])
-                        flag = true;
-                }
-                if (flag == true)
-                {
-                    Count++;
-                    flag = false;
-                }
-                else
-                    Missnumber = Count;
-            }
-            return Missnumber;
-        }
-#endif
 
 #if FacebookLiker
             try
@@ -144,27 +112,92 @@ internal class Program
             }
 #endif
 
-#if ReplaceLetters           
-            string str = "qwertyu";
-            if (str.Length % 2 != 0)
-                str += '_';
-            for (int i = 0; i < str.Length - 2;)
+#if Origami
+            int a = 10;
+            int b = 7;
+            int sum = 0;
+            while (a > 0 && b > 0)
             {
-                i += 2;
-                str = str.Insert(i, " ");
-                i++;
+                if (a > b)
+                {
+                    a -= b;
+                    sum++;
+                }
+                else
+                {
+                    b -= a;
+                    sum++;
+                }
             }
-            string[] Splited = str.Split(' ');
-            for (int i = 0;i < Splited.Length;i++)
-                Console.WriteLine(Splited[i]);        
+            Console.WriteLine(sum);
+            Console.ReadKey();
+#endif
+
+#if HighandLow
+            string Numbers = "1 2 3 4 5 6 7 8";
+            int low = int.MaxValue;
+            int high = int.MinValue;
+          string[]  Cache= Numbers.Split(' ');
+            Numbers = null;
+            for (int i = 0; i < Cache.Length; i++)
+            {
+             if (Convert.ToInt32(Cache[i]) < low)
+                low = Convert.ToInt32(Cache[i]);
+             if (Convert.ToInt32(Cache[i]) > high)
+                    high = Convert.ToInt32(Cache[i]);
+            }
+            Numbers = high + " " + low;
+            Console.WriteLine(Numbers);
+            Console.ReadKey();
+#endif
+
+#if Test
+            int [] Numbers = {  1, 2, 3, 4,5, 6, 7, 8 };
+            for (int i = 0; i <= Numbers.Length; i++)
+                if (!Numbers.Contains(i))
+                    Console.WriteLine(i);
             Console.ReadKey();
 #endif
 
 
 
-
-
+        }
+#if ArrayChecker
+        static void ArrayChecker(string[] args)
+        {
+            int[] arr1 = new[]
+          {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+            28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+            80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+        }; // 100
+            
+            Console.WriteLine(MissingNo(arr1));
+            Console.ReadKey();
+        }
+        public static int MissingNo(int[] nums)
+        {
+            int Count = 0, Missnumber = 0;
+            bool flag = false;
+            for (int i = 0; i < nums.Length + 1; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (Count == nums[j])
+                        flag = true;
+                }
+                if (flag == true)
+                {
+                    Count++;
+                    flag = false;
+                }
+                else
+                    Missnumber = Count;
+            }            
+            return Missnumber;
 
         }
+#endif
 }
 }
